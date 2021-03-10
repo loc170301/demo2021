b2ca6545a70a194a03a71b2698c7cf54c81d7cd6
congloc90 <congloc90@gmail.com>
2021-03-10 15:04:33 +0700

    add api conroller model

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todos;
+
+class TodosController extends Controller
+{
+    public function index(){
+        $todos = Todos::all();
+        return view('Todos.todos_list', compact('todos'));
+    }
+
+    public function store(Request $request)
+    {
+        $todos = new Todos();
+        $todos->notes = $request->notes;
+        $todos->status = 0;
+        $todos->save();
+
+        return response()->json($todos, 201);
+    }
+
+    public function show($id){
+        $todo = Todos::find($id);
+        return response()->json($todos);
+    }
+
+    public function update($id,Request $request){
+        $todo = Todos::find($id);
+
+        $todo->notes = $request->post('notes');
+
+        if ($request->has('status')) {
+            $todo->status = $request->post('status');
+        }
+        $todo->save();
+
+        return response()->json($todo);
+    }
+
+    public function delete($id){
+        Todos::where('id',$id)->delete();
+        return response(null, 204);
+    }
+
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/todos', [TodosController::class, 'store'])->name('todos.store');
+Route::get('/todos/{id}', [TodosController::class, 'show'])->name('todos_show');
+Route::put('/todos/{id}', [TodosController::class, 'update'])->name('todos.update');
+Route::delete('/todos/{id}', [TodosController::class, 'delete'])->name('todos_delete');
+
+
+
+
+
+
+
